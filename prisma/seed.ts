5b17bc0579c3706c48b621da9c09da3e6ec52374
pray3m <prem.gtm9@gmail.com>
2024-11-09 23:19:36 +0545

    Add a User model to the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,54 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 // initialize Prisma client
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy articles
-  const post1 = await prisma.article.upsert({
-    where: { title: 'Prisma is awesome' },
+  // create two dummy users
+  const user1 = await prisma.user.upsert({
+    where: { email: "sabin@adams.com" },
     update: {},
     create: {
-      title: 'Prisma is awesome',
-      body: 'Prisma is a modern database toolkit that makes it easy to work with databases.',
-      description: 'Prisma is a modern database toolkit',
+      email: "sabin@adams.com",
+      name: "Sabin Adams",
+      password: "password-sabin",
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: "alex@ruheni.com" },
+    update: {},
+    create: {
+      email: "alex@ruheni.com",
+      name: "Alex Ruheni",
+      password: "password-alex",
+    },
+  });
+
+  // create two dummy articles
+  const post1 = await prisma.article.upsert({
+    where: { title: "Prisma is awesome" },
+    update: {},
+    create: {
+      title: "Prisma is awesome",
+      body: "Prisma is a modern database toolkit that makes it easy to work with databases.",
+      description: "Prisma is a modern database toolkit",
       isPublished: false,
     },
   });
 
   const post2 = await prisma.article.upsert({
-    where: { title: 'Prisma supports MongoDB' },
+    where: { title: "Prisma supports MongoDB" },
     update: {},
     create: {
-      title: 'Prisma supports MongoDB',
-      body: 'Prisma supports MongoDB and other databases.',
-      description: 'Prisma supports MongoDB',
+      title: "Prisma supports MongoDB",
+      body: "Prisma supports MongoDB and other databases.",
+      description: "Prisma supports MongoDB",
       isPublished: false,
     },
   });
 
-  console.log({ post1, post2 });
+  console.log({ user1, user2, post1, post2 });
 }
 
 // execute the main function
